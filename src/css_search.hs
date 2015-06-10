48e32134726b1ee71f4d3f321e513b403e91f533
Whatisinternet <joshteeter@gmail.com>
2015-06-09 22:26:13 -0400

    Moved load files into function (not working 100%) Added some testData

--- src/css_search.hs
+++ src/css_search.hs
@@ -11,15 +11,20 @@ import Data.Foldable
 
 main :: IO ()
 main = do
-  css <- readFile "testData/a.css"
+  css <- loadFiles ["testData/a.css", "testData/b.css"]
   html <- readFile "testData/test.html"
   let cssClasses = extractCSS css
-  let remainingClasses = filterCSS cssClasses html
+  let remainingClasses = filterCSS (cssClasses, html)
   print remainingClasses
+
+loadFiles :: [String] -> IO String
+loadFiles fileNames = do
+    output <- head (map readFile fileNames)
+    return output
 
 extractCSS :: String -> [String]
 extractCSS css = (map head ((css)  =~ "[.][a-zA-Z][ A-Za-z_-]*" :: [[String]]))
 
 filterCSS :: ([String], String) -> [String]
-filterCSS [ x| x <- cssClasses, x `notElem` (filter (html=~) cssClasses)]
+filterCSS (cssClasses, html) = [ x| x <- cssClasses, x `notElem` (filter (html=~) cssClasses)]
 
